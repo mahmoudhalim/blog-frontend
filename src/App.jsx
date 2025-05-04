53a7bcdacb1b9eb407163d600ab2e3e49ba5b50a
mahmoud isa <mahmoudahalim0@gmail.com>
2025-05-04 16:09:40 +0300

    feat: add Notification bar
    Implemented notifications that inform the user about successful and unsuccessful operations at the top of the page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
+import { Notification } from './components/Notification'
 import blogService from './services/blogs'
 import loginService from './services/login'
 
@@ -8,9 +9,12 @@ const App = () => {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
-  const [title, setTitle] = useState("")
-  const [author, setAuthor] = useState("")
-  const [url, setUrl] = useState("")
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [url, setUrl] = useState('')
+  const [notificationMessage, setNotificationMessage] = useState({
+    message: '',
+  })
   useEffect(() => {
     blogService.getAll().then((blogs) => setBlogs(blogs))
   }, [])
@@ -21,10 +25,14 @@ const App = () => {
       const user = JSON.parse(loggedUser)
       setUser(user)
       blogService.setToken(user.token)
-
     }
   }, [])
 
+
+  const changeNotificationMessage = (notification) => {
+    setNotificationMessage(notification)
+    setTimeout(() => setNotificationMessage({message:""}), 5000)
+  }
   const handleLogin = async (e) => {
     e.preventDefault()
     try {
@@ -34,7 +42,15 @@ const App = () => {
       setUser(user)
       setUsername('')
       setPassword('')
+      changeNotificationMessage({
+        message: `Welcome ${user.name}!`,
+        isError: true,
+      })
     } catch (exception) {
+      changeNotificationMessage({
+        message: 'wrong name or password',
+        isError: false,
+      })
       console.log('Wrong credentials')
     }
   }
@@ -48,20 +64,33 @@ const App = () => {
       title,
       author,
       url,
-  }
-  try {
-        const response = await blogService.create(newblog)
-        setTitle('')
-        setAuthor('')
-        setUrl('')
-        setBlogs(blogs.concat(response))
-  } catch (error) {
-    console.log(error.response.data)
-  }
+    }
+    try {
+      const response = await blogService.create(newblog)
+      setTitle('')
+      setAuthor('')
+      setUrl('')
+      setBlogs(blogs.concat(response))
+      changeNotificationMessage({
+        message: `a new blog has been added!`,
+        isError: false,
+      })
+    } catch (e) {
+      
+      changeNotificationMessage({
+        message: e.response.data.error,
+        isError: true,
+      })
+      console.log(e.response.data)
+    }
   }
   if (user === null)
     return (
       <div>
+        <Notification
+          message={notificationMessage.message}
+          isError={notificationMessage.isError}
+        />
         <h2>Login</h2>
         <form onSubmit={handleLogin}>
           <div>
@@ -89,11 +118,15 @@ const App = () => {
   else
     return (
       <div>
+        <Notification
+          message={notificationMessage.message}
+          isError={notificationMessage.isError}
+        />
         <h2>blogs</h2>
         {user.name} is logged in
         <button onClick={() => handleLogout()}>Logout</button>
         <h2>add new blog</h2>
-        <form onSubmit={handleNewBlog} method='POST'>
+        <form onSubmit={handleNewBlog} method="POST">
           <div>
             title:
             <input
