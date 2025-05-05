c4044bc02e78ae7b3dbe525ff92ab21599d7d63f
mahmoud isa <mahmoudahalim0@gmail.com>
2025-05-05 18:46:39 +0300

    feat: added a button to each blog, which controls whether all of the details about the blog are shown or not
    also added some styling

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,7 +1,28 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [showDetails, setShowDetails] = useState(false)
+
+  
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5,
+  }
+  return (
+    <div style={blogStyle}>
+      {blog.title}
+      <button onClick={() => setShowDetails(!showDetails)}>
+        {showDetails ? 'hide' : 'show'}
+      </button>
+      <div style={{ display: showDetails ? '' : 'none' }}>
+        <div>{blog.author}</div>
+        <div>{blog.url}</div>
+      </div>
+    </div>
+  )
+}
 
 export default Blog
